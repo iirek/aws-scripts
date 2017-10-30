9d66722a9bbb3f50899d0f671cf3860ab464183f
iirek <l2irro@gmail.com>
2017-10-30 16:48:06 +0000

    Few tweaks around validation

--- main.go
+++ main.go
@@ -4,22 +4,37 @@ import (
 	"fmt"
 	"os"
 
-	_ "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws"
 	_ "github.com/aws/aws-sdk-go/aws/awserr"
 	_ "github.com/aws/aws-sdk-go/aws/request"
-	_ "github.com/aws/aws-sdk-go/aws/session"
-	_ "github.com/aws/aws-sdk-go/service/elasticache"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/elasticache"
 )
 
 func main() {
-	verifyEnvVariables()
+	fmt.Printf("Are variable verified? %s", verifyEnvVariables())
+	
+	sess := session.Must(session.NewSession())
+
+	svc := elasticache.New(sess, aws.NewConfig().WithRegion("eu-west-1"))
+
+	fmt.Printf("svc is %", svc.ClientInfo)
+
 }
 
-func verifyEnvVariables() {
+func verifyEnvVariables()(isVerified bool)  {
 	variables := []string {"AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_SESSION_TOKEN"}
+	var verifiedLen = 0
 
 	for _, variable := range variables {
-		fmt.Printf("VARIABLE :%s is: %s", variable , os.Getenv(variable))
+		var envVariable = os.Getenv(variable)
+		if len (envVariable) > 0 {
+			verifiedLen +=1
+		}
 	}
+	isVerified = verifiedLen == len(variables)
+
+	return
+
 
 }
